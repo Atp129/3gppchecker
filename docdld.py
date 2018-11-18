5b54ad0f70980b3f06685b33fec5cca90f04271c
atp129 <atp129@126.com>
2018-11-18 11:54:17 +0800

    filedownloader

---
+++ docdld.py
@@ -0,0 +1,14 @@
+#
+import requests
+
+def filedld(url, name):
+    print "downloading with requests"
+    # url = 'http://ww.pythontab.com/test/demo.zip'
+    r = requests.get(url)
+    with open(name, "wb") as code:
+        code.write(r.content)
+
+if __name__ == "__main__":
+    url = "http://www.3gpp.org/ftp/Specs/archive/38_series/38.331/38331-f30.zip"
+    name = "38331-f30.zip"
+    filedld(url, name)
