c55ac03e076e759af3c72620690807f616521c4d
atp129 <atp129@126.com>
2019-03-19 23:16:14 +0800

    filedownloader

---
+++ 3gppdoc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/3 15:34
+# @Author  : Hongkang LI
+# @Site    : 
+# @File    : 3gppdoc.py
+# @Software: 3gppchecker
+
+
+class SpecDoc(object):
+    def __init__(self, name):
+        self.name = ''
+        self.description = ''
+
+
+
+class DocFormat(object):
+    def __init__(self):
+        self.title = "Title"
---
+++ asncode.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/3 15:33
+# @Author  : Hongkang LI
+# @Site    : 
+# @File    : asncode.py
+# @Software: 3gppchecker
+
